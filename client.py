4f9a3efe7f7e97ead212ef28c27c7acb0a86af3e
ASA-D <83509392+ASA-D@users.noreply.github.com>
2021-08-29 13:43:28 +0900

    Update client.py

--- client.py
+++ client.py
@@ -5,5 +5,7 @@ a = random.randint(1, 26)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 100))
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+msg = s.recv(1024)
+print(msg.decode("utf-8"))
 
 print(a)
